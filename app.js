49ebb919ba7f4139639b864de74f997ae2f114fb
Daniel Andŕes Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-07-19 14:21:11 -0500

    [UPDATE] Test querys and mutations added to the api

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql')
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql").graphqlHTTP;
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 // The endpoint is commonly named "graphql", but it could be named whatever you want.
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-        
+            events: [Event!]!
         }
 
         type RootMutation {
-        
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -23,7 +40,17 @@ app.use('/graphql', graphqlHttp({
             mutation: RootMutation
         }
         `),
-    rootValue: {},
-}));
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "All-Nigth Coding"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(3000);
